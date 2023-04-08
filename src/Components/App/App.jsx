80c1ef11e2563b3cd3870b0008a380abfb063247
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-04-08 09:50:41 -0500

    finish up

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,16 +1,15 @@
 import { useState } from 'react'
 import './App.sass'
+import Jokes from '../../Containers/Jokes/Jokes'
 
-import snesController from '../../assets/supernintendo.png'
-
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div className="App-container">
-        <h1>Dokes V2.0</h1>
-        <img src={snesController} alt='super nintendo controller' />
+        <h1>Dokes V2</h1>
+        <Jokes />
       </div>
     </>
   )
---
+++ src/Containers/Jokes/Jokes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Jokes.sass'
+
+const Jokes = () => {
+  return (
+    <div className='joke-container'>
+      jkdfjsljdsajfkdjfdjlfj
+    </div>
+  )
+}
+
+export default Jokes
